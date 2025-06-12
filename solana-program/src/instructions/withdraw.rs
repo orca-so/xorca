400a9dd25854c6dd6cfe8f0da9e089fdb304cec3
josh-orca <jguha@orca.so>
2025-06-12 20:51:21 +0100

    [Withdraw][2] Add withdraw instruction implementation

--- solana-program/src/instructions/withdraw.rs
+++ solana-program/src/instructions/withdraw.rs
@@ -3,13 +3,15 @@ use crate::{
         assert_account_address, assert_account_data_mut, assert_account_owner, assert_account_role,
         assert_account_seeds, make_owner_token_account_assertions, AccountRole,
     },
+    cpi::system::get_current_unix_timestamp,
+    error::ErrorCode,
     state::{pending_withdraw::PendingWithdraw, staking_pool::StakingPool},
     util::account::get_account_info,
 };
 use pinocchio::{account_info::AccountInfo, instruction::Seed, ProgramResult};
 use pinocchio_associated_token_account::ID as ASSOCIATED_TOKEN_PROGRAM_ID;
 use pinocchio_system::ID as SYSTEM_PROGRAM_ID;
-use pinocchio_token::ID as SPL_TOKEN_PROGRAM_ID;
+use pinocchio_token::{instructions::Transfer, ID as SPL_TOKEN_PROGRAM_ID};
 
 pub fn process_instruction(accounts: &[AccountInfo], withdraw_index: &u8) -> ProgramResult {
     let unstaker_account = get_account_info(accounts, 0)?;
@@ -85,5 +87,25 @@ pub fn process_instruction(accounts: &[AccountInfo], withdraw_index: &u8) -> Pro
     // 8. Token Program Account Assertions
     assert_account_address(token_program_account, &SPL_TOKEN_PROGRAM_ID)?;
 
+    // Validate pending withdraw
+    let current_unix_timestamp = get_current_unix_timestamp()?;
+    if current_unix_timestamp < pending_withdraw_data.withdrawable_timestamp {
+        return Err(ErrorCode::InvalidAccountData.into());
+    }
+
+    // Transfer withdrawable stake tokens from staking pool to unstaker
+    let transfer_instruction = Transfer {
+        from: staking_pool_stake_token_account,
+        to: unstaker_stake_token_account,
+        authority: staking_pool_account,
+        amount: pending_withdraw_data.withdrawable_stake_amount,
+    };
+    transfer_instruction.invoke_signed(&[staking_pool_seeds.as_slice().into()])?;
+
+    // Remove tokens from escrow
+    staking_pool_data.escrowed_stake_token_amount -=
+        pending_withdraw_data.withdrawable_stake_amount;
+
+    // TODO: Close Pending Withdraw Account
     Ok(())
 }
