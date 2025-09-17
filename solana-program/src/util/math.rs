7146ab39b6015ed190772fcfb63633d69e5d8cd2
josh-orca <jguha@orca.so>
2025-09-17 20:00:58 +0100

    [Vault Inflation][3] Add virtual assets to exchange rate calculations to guarantee loss to vault inflation attacker

--- solana-program/src/util/math.rs
+++ solana-program/src/util/math.rs
@@ -1,6 +1,9 @@
 use crate::error::ErrorCode;
 use pinocchio::program_error::ProgramError;
 
+static VIRTUAL_XORCA_SUPPLY: u128 = 1;
+static VIRTUAL_NON_ESCROWED_ORCA_AMOUNT: u128 = 1;
+
 pub fn convert_orca_to_xorca(
     orca_amount_to_convert: u64,
     non_escrowed_orca_amount: u64,
@@ -16,10 +19,19 @@ pub fn convert_orca_to_xorca(
     let orca_amount_to_convert_u128 = orca_amount_to_convert as u128;
     let non_escrowed_orca_amount_u128 = non_escrowed_orca_amount as u128;
 
+    // Add virtual assets to the xORCA supply and non-escrowed ORCA amount to disincentivize vault inflation attack.
+    // (See https://docs.openzeppelin.com/contracts/4.x/erc4626#defending_with_a_virtual_offset)
+    let xorca_supply_with_virtual_amount_u128 = xorca_supply_u128
+        .checked_add(VIRTUAL_XORCA_SUPPLY)
+        .ok_or(ErrorCode::ArithmeticError)?;
+    let non_escrowed_orca_amount_with_virtual_amount_u128 = non_escrowed_orca_amount_u128
+        .checked_add(VIRTUAL_NON_ESCROWED_ORCA_AMOUNT)
+        .ok_or(ErrorCode::ArithmeticError)?;
+
     let out_xorca_amount_u128 = orca_amount_to_convert_u128
-        .checked_mul(xorca_supply_u128)
+        .checked_mul(xorca_supply_with_virtual_amount_u128)
         .ok_or(ErrorCode::ArithmeticError)?
-        .checked_div(non_escrowed_orca_amount_u128)
+        .checked_div(non_escrowed_orca_amount_with_virtual_amount_u128)
         .ok_or(ErrorCode::ArithmeticError)?;
 
     // Cast the final u128 result back to u64.
@@ -46,10 +58,19 @@ pub fn convert_xorca_to_orca(
     let xorca_amount_to_convert_u128 = xorca_amount_to_convert as u128;
     let xorca_supply_u128 = xorca_supply as u128;
 
+    // Add virtual assets to the xORCA supply and non-escrowed ORCA amount to disincentivize vault inflation attack.
+    // (See https://docs.openzeppelin.com/contracts/4.x/erc4626#defending_with_a_virtual_offset)
+    let xorca_supply_with_virtual_amount_u128 = xorca_supply_u128
+        .checked_add(VIRTUAL_XORCA_SUPPLY)
+        .ok_or(ErrorCode::ArithmeticError)?;
+    let non_escrowed_orca_amount_with_virtual_amount_u128 = non_escrowed_orca_amount_u128
+        .checked_add(VIRTUAL_NON_ESCROWED_ORCA_AMOUNT)
+        .ok_or(ErrorCode::ArithmeticError)?;
+
     let out_orca_amount_u128 = xorca_amount_to_convert_u128
-        .checked_mul(non_escrowed_orca_amount_u128)
+        .checked_mul(non_escrowed_orca_amount_with_virtual_amount_u128)
         .ok_or(ErrorCode::ArithmeticError)?
-        .checked_div(xorca_supply_u128)
+        .checked_div(xorca_supply_with_virtual_amount_u128)
         .ok_or(ErrorCode::ArithmeticError)?;
 
     // Cast the final u128 result back to u64.
