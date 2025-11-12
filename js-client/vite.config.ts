1532d0c1da19594aeac0496ade23fab0fcb687d9
calintje <calsim113@gmail.com>
2025-11-12 12:00:11 +0000

    Alias solana assertions

--- js-client/vite.config.ts
+++ js-client/vite.config.ts
@@ -48,7 +48,20 @@ export default defineConfig(({ mode }) => {
       target,
       emptyOutDir: false,
     },
-    resolve: { alias: { src: resolve('src/') } },
+    resolve: {
+      alias: {
+        src: resolve("src/"),
+        // resolve.conditions doesn't work well for @solana/assersions (and other @solana/* packages)
+        // To avoid the following error:
+        // [SolanaError]: Cryptographic operations are only allowed in secure browser contexts. Read more here: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts.
+        "@solana/assertions":
+          mode === "node"
+            ? resolve("../node_modules/@solana/assertions/dist/index.node.mjs")
+            : resolve(
+              "../node_modules/@solana/assertions/dist/index.browser.mjs",
+            ),
+      },
+    },
     test: {
       includeSource: ['**/*.{js,ts}'],
     },
